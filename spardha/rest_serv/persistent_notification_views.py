ade09a88ee5923592803ef9815373138de47a11a
Shubham Avasthi <shubhamavasthi1@gmail.com>
2017-06-10 21:07:55 +0530

    Add files via upload

--- spardha/rest_serv/persistent_notification_views.py
+++ spardha/rest_serv/persistent_notification_views.py
@@ -4,10 +4,11 @@ from rest_framework.parsers import JSONParser
 import json
 from rest_serv.models import PersistentNotification
 from rest_serv.serializers import PersistentNotificationSerializer
+from django.views.decorators.http import require_GET
 
+@require_GET
 def persistent_notification_list(request):
     if request.method == 'GET':
         persistent_notifications = PersistentNotification.objects.all()
         serializer = PersistentNotificationSerializer(persistent_notifications, many=True)
         return JsonResponse(serializer.data, safe=False)
-    return HttpResponse(json.dumps({"error":"true","detail":"bad_request"}), status=400, content_type="application/json")

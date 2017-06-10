4b1810ba5e6f16c6f2a89ee7b429ea5e7960aa42
Shubham Avasthi <shubhamavasthi1@gmail.com>
2017-06-11 04:12:13 +0530

    Add files via upload

---
+++ spardha/rest_serv/event_views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+import json
+from rest_serv.models import Event
+from rest_serv.serializers import EventSerializer
+from django.views.decorators.http import require_GET
+
+@require_GET	
+def event_view(request):
+    events = Event.objects.all()
+    serializer = EventSerializer(events, many=True)
+    return JsonResponse(serializer.data, safe=False)
